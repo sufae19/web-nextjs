0a7a4bd2788c1245d2823c4800010242b4afcabe
sufae19 <ppsbanjarnegara@gmail.com>
2024-12-16 18:57:40 +0700

    first commit

---
+++ app/about/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function page(){
+  
+  return (
+    <div>
+        <h1 className='text-2x1 font-bold'>About page</h1>
+        <a href="http://localhost:3000">Home page</a>
+    </div>
+  )
+}
---
+++ components/HelloWorld.tsx
@@ -0,0 +1,14 @@
+function HelloWorld() {
+  return (
+    <div>
+        <h1>hello world</h1>
+
+        <p>selamat belajar nextjs</p>
+
+        <button>Login</button>
+    </div>
+    
+  )
+}
+
+export default HelloWorld
